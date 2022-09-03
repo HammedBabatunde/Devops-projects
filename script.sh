e78bc2db2cf117a0948f808e15d18d86b94a9f90
Hammed Babatunde <dev_babs@pop-os.localdomain>
2022-09-04 00:14:20 +0100

    Updated script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo apt update
-sudo apt install apache2
+sudo apt update -y
+sudo apt install apache2 -y
 git clone https://github.com/HammedBabatunde/Devops-projects.git
 cd Devops-projects
 sudo cp webcontent/* /var/www/html/
